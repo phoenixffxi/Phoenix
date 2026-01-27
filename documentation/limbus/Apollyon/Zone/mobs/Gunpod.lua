0708197aa1db094b7cc00af107fdddd0a17823bd
Skold177 <113406182+Skold177@users.noreply.github.com>
2026-01-27 11:59:42 -0500

    [lua] Central Apollyon loot audit
    Cleans up and corrects Central Apollyon loot pools

--- documentation/limbus/Apollyon/Zone/mobs/Gunpod.lua
+++ documentation/limbus/Apollyon/Zone/mobs/Gunpod.lua
@@ -15,52 +15,52 @@ entity.onMobInitialize = function(mob)
             -- Apollyon Chips
             group =
             {
-                { item = xi.item.SMALT_CHIP },
-                { item = xi.item.SMOKY_CHIP },
-                { item = xi.item.CHARCOAL_CHIP },
-                { item = xi.item.MAGENTA_CHIP },
+                { item = xi.item.SMALT_CHIP    }, -- SE Apollyon
+                { item = xi.item.SMOKY_CHIP    }, -- NE Apollyon
+                { item = xi.item.CHARCOAL_CHIP }, -- SW Apollyon
+                { item = xi.item.MAGENTA_CHIP  }, -- NW Apollyon
             }
         elseif result <= 50 then
-            -- Craft Materials
+            -- Crafting Materials
             group =
             {
-                { item = xi.item.CHUNK_OF_DARKSTEEL_ORE },
-                { item = xi.item.CHUNK_OF_ADAMAN_ORE },
-                { item = xi.item.DARKSTEEL_INGOT },
-                { item = xi.item.DARKSTEEL_SHEET },
+                { item = xi.item.CHUNK_OF_DARKSTEEL_ORE  },
+                { item = xi.item.CHUNK_OF_ADAMAN_ORE     },
+                { item = xi.item.DARKSTEEL_INGOT         },
+                { item = xi.item.DARKSTEEL_SHEET         },
                 { item = xi.item.SPOOL_OF_RAINBOW_THREAD },
-                { item = xi.item.PIECE_OF_OXBLOOD },
-                { item = xi.item.HANDFUL_OF_CLOT_PLASMA },
-                { item = xi.item.LIGHT_STEEL_INGOT },
-                { item = xi.item.PONZE_OF_SHELL_POWDER },
+                { item = xi.item.PIECE_OF_OXBLOOD        },
+                { item = xi.item.HANDFUL_OF_CLOT_PLASMA  },
+                { item = xi.item.LIGHT_STEEL_INGOT       },
+                { item = xi.item.PONZE_OF_SHELL_POWDER   },
             }
         elseif result <= 86 then
             -- AF+1 Materials
             group =
             {
-                { item = xi.item.ARGYRO_RIVET },
-                { item = xi.item.ANCIENT_BRASS_INGOT },
-                { item = xi.item.SPOOL_OF_BENEDICT_YARN },
-                { item = xi.item.SPOOL_OF_DIABOLIC_YARN },
-                { item = xi.item.SQUARE_OF_CARDINAL_CLOTH },
-                { item = xi.item.SPOOL_OF_LIGHT_FILAMENT },
-                { item = xi.item.WHITE_RIVET },
-                { item = xi.item.BLACK_RIVET },
-                { item = xi.item.FETID_LANOLIN_CUBE },
-                { item = xi.item.SQUARE_OF_BROWN_DOESKIN },
-                { item = xi.item.SQUARE_OF_CHARCOAL_COTTON },
-                { item = xi.item.SHEET_OF_KUROGANE },
-                { item = xi.item.POT_OF_EBONY_LACQUER },
-                { item = xi.item.BLUE_RIVET },
-                { item = xi.item.SQUARE_OF_ASTRAL_LEATHER },
-                { item = xi.item.SQUARE_OF_FLAMESHUN_CLOTH },
-                { item = xi.item.SQUARE_OF_CANVAS_TOILE },
-                { item = xi.item.SQUARE_OF_CORDUROY_CLOTH },
-                { item = xi.item.GOLD_STUD },
-                { item = xi.item.ELECTRUM_STUD },
+                { item = xi.item.ARGYRO_RIVET              }, -- WAR
+                { item = xi.item.ANCIENT_BRASS_INGOT       }, -- MNK
+                { item = xi.item.SPOOL_OF_BENEDICT_YARN    }, -- WHM
+                { item = xi.item.SPOOL_OF_DIABOLIC_YARN    }, -- BLM
+                { item = xi.item.SQUARE_OF_CARDINAL_CLOTH  }, -- RDM
+                { item = xi.item.SPOOL_OF_LIGHT_FILAMENT   }, -- THF
+                { item = xi.item.WHITE_RIVET               }, -- PLD
+                { item = xi.item.BLACK_RIVET               }, -- DRK
+                { item = xi.item.FETID_LANOLIN_CUBE        }, -- BST
+                { item = xi.item.SQUARE_OF_BROWN_DOESKIN   }, -- BRD
+                { item = xi.item.SQUARE_OF_CHARCOAL_COTTON }, -- RNG
+                { item = xi.item.SHEET_OF_KUROGANE         }, -- SAM
+                { item = xi.item.POT_OF_EBONY_LACQUER      }, -- NIN
+                { item = xi.item.BLUE_RIVET                }, -- DRG
+                { item = xi.item.SQUARE_OF_ASTRAL_LEATHER  }, -- SMN
+                { item = xi.item.SQUARE_OF_FLAMESHUN_CLOTH }, -- BLU
+                { item = xi.item.SQUARE_OF_CANVAS_TOILE    }, -- COR
+                { item = xi.item.SQUARE_OF_CORDUROY_CLOTH  }, -- PUP
+                --{ item = xi.item.GOLD_STUD               }, -- DNC
+                --{ item = xi.item.ELECTRUM_STUD           }, -- SCH
             }
         else
-            -- Acient Beastcoins
+            -- Ancient Beastcoins
             loot:addItem(xi.item.ANCIENT_BEASTCOIN, xi.drop_rate.GUARANTEED, 5)
             loot:addItem(xi.item.ANCIENT_BEASTCOIN, xi.drop_rate.COMMON)
             return
