8878944eeb1cc11f2eaabc9e6a13d659aef6f033
Skold177 <113406182+Skold177@users.noreply.github.com>
2026-04-09 13:21:44 -0400

    [lua] [sql] KS99 Adjustments
    Converts loot pools to weights out of 10,000
    Adds missing immunities, damage multipliers, corrects delay, adds lua spell and skill lists.

--- scripts/battlefields/Waughroon_Shrine/hills_are_alive.lua
+++ scripts/battlefields/Waughroon_Shrine/hills_are_alive.lua
@@ -22,104 +22,106 @@ content:addEssentialMobs({ 'Tartaruga_Gigante' })
 content.loot =
 {
     {
-        { itemId = xi.item.GIL, weight = 1000, amount = 32000 }, -- Gil
+        { itemId = xi.item.GIL,                        weight = 10000, amount = 32000 },
     },
 
     {
-        { itemId = xi.item.CLUMP_OF_BLUE_PONDWEED, weight = 1000 }, -- Blue Pondweed
+        { itemId = xi.item.CLUMP_OF_BLUE_PONDWEED,     weight = 10000 },
     },
 
     {
-        { itemId = xi.item.HAVOC_SCYTHE,        weight = 188 }, -- Havoc Scythe
-        { itemId = xi.item.KRIEGSBEIL,          weight =  27 }, -- Kriegsbeil
-        { itemId = xi.item.LEOPARD_AXE,         weight = 170 }, -- Leopard Axe
-        { itemId = xi.item.LIBATION_ABJURATION, weight = 295 }, -- Libation Abjuration
-        { itemId = xi.item.METEOR_CESTI,        weight =  27 }, -- Meteor Cesti
-        { itemId = xi.item.PURGATORY_MACE,      weight =  71 }, -- Purgatory Mace
-        { itemId = xi.item.SOMNUS_SIGNA,        weight = 196 }, -- Somnus Signa
+        { itemId = xi.item.LIBATION_ABJURATION,        weight =  2500 },
+        { itemId = xi.item.HAVOC_SCYTHE,               weight =  1250 },
+        { itemId = xi.item.KRIEGSBEIL,                 weight =  1250 },
+        { itemId = xi.item.LEOPARD_AXE,                weight =  1250 },
+        { itemId = xi.item.METEOR_CESTI,               weight =  1250 },
+        { itemId = xi.item.PURGATORY_MACE,             weight =  1250 },
+        { itemId = xi.item.SOMNUS_SIGNA,               weight =  1250 },
     },
 
     {
-        { itemId = xi.item.GAWAINS_AXE,         weight =  45 }, -- Gawains Axe
-        { itemId = xi.item.GRIM_STAFF,          weight = 259 }, -- Grim Staff
-        { itemId = xi.item.GROSVENEURS_BOW,     weight = 241 }, -- Grosveneurs Bow
-        { itemId = xi.item.HARLEQUINS_HORN,     weight = 143 }, -- Harlequins Horn
-        { itemId = xi.item.OBLATION_ABJURATION, weight = 161 }, -- Oblation Abjuration
-        { itemId = xi.item.STYLET,              weight = 143 }, -- Stylet
-        { itemId = xi.item.ZEN_POLE,            weight =  36 }, -- Zen Pole
+        { itemId = xi.item.OBLATION_ABJURATION,        weight =  2500 },
+        { itemId = xi.item.GAWAINS_AXE,                weight =  1250 },
+        { itemId = xi.item.GRIM_STAFF,                 weight =  1250 },
+        { itemId = xi.item.GROSVENEURS_BOW,            weight =  1250 },
+        { itemId = xi.item.HARLEQUINS_HORN,            weight =  1250 },
+        { itemId = xi.item.STYLET,                     weight =  1250 },
+        { itemId = xi.item.ZEN_POLE,                   weight =  1250 },
     },
 
     {
-        { itemId = xi.item.ADAMAN_CHAIN,        weight = 446 }, -- Adaman Chain
-        { itemId = xi.item.ADAMANTOISE_SHELL,   weight = 420 }, -- Adamantoise Shell
-        { itemId = xi.item.PIECE_OF_ANGEL_SKIN, weight =  71 }, -- Piece Of Angel Skin
-        { itemId = xi.item.STRIDER_BOOTS,       weight =  26 }, -- Strider Boots
+        { itemId = xi.item.ADAMAN_CHAIN,               weight =  4500 },
+        { itemId = xi.item.ADAMANTOISE_SHELL,          weight =  4250 },
+        { itemId = xi.item.PIECE_OF_ANGEL_SKIN,        weight =   750 },
+        { itemId = xi.item.STRIDER_BOOTS,              weight =   500 },
     },
 
     {
-        { itemId = xi.item.CORAL_FRAGMENT,           weight = 116 }, -- Coral Fragment
-        { itemId = xi.item.CHUNK_OF_DARKSTEEL_ORE,   weight =  89 }, -- Chunk Of Darksteel Ore
-        { itemId = xi.item.DEMON_HORN,               weight =  71 }, -- Demon Horn
-        { itemId = xi.item.EBONY_LOG,                weight = 152 }, -- Ebony Log
-        { itemId = xi.item.CHUNK_OF_GOLD_ORE,        weight = 107 }, -- Chunk Of Gold Ore
-        { itemId = xi.item.SPOOL_OF_GOLD_THREAD,     weight =  89 }, -- Spool Of Gold Thread
-        { itemId = xi.item.SLAB_OF_GRANITE,          weight =  45 }, -- Slab Of Granite
-        { itemId = xi.item.HI_RERAISER,              weight =  71 }, -- Hi-reraiser
-        { itemId = xi.item.MAHOGANY_LOG,             weight = 107 }, -- Mahogany Log
-        { itemId = xi.item.PETRIFIED_LOG,            weight = 223 }, -- Petrified Log
-        { itemId = xi.item.CHUNK_OF_PLATINUM_ORE,    weight = 116 }, -- Chunk Of Platinum Ore
-        { itemId = xi.item.SQUARE_OF_RAINBOW_CLOTH,  weight =  54 }, -- Square Of Rainbow Cloth
-        { itemId = xi.item.RAM_HORN,                 weight =  54 }, -- Ram Horn
-        { itemId = xi.item.SQUARE_OF_RAXA,           weight =  71 }, -- Square Of Raxa
-        { itemId = xi.item.RERAISER,                 weight =  45 }, -- Reraiser
-        { itemId = xi.item.HANDFUL_OF_WYVERN_SCALES, weight =  54 }, -- Handful Of Wyvern Scales
-        { itemId = xi.item.VILE_ELIXIR,              weight =  63 }, -- Vile Elixir
-        { itemId = xi.item.VILE_ELIXIR_P1,           weight =  45 }, -- Vile Elixir +1
+        { itemId = xi.item.CHUNK_OF_DARKSTEEL_ORE,     weight =   500 },
+        { itemId = xi.item.CHUNK_OF_GOLD_ORE,          weight =   500 },
+        { itemId = xi.item.CHUNK_OF_MYTHRIL_ORE,       weight =   500 },
+        { itemId = xi.item.CHUNK_OF_PLATINUM_ORE,      weight =   500 },
+        { itemId = xi.item.EBONY_LOG,                  weight =   500 },
+        { itemId = xi.item.MAHOGANY_LOG,               weight =   500 },
+        { itemId = xi.item.PETRIFIED_LOG,              weight =   500 },
+        { itemId = xi.item.PHILOSOPHERS_STONE,         weight =   500 },
+        { itemId = xi.item.SPOOL_OF_GOLD_THREAD,       weight =   500 },
+        { itemId = xi.item.SQUARE_OF_RAINBOW_CLOTH,    weight =   500 },
+        { itemId = xi.item.SQUARE_OF_RAXA,             weight =   500 },
+        { itemId = xi.item.CORAL_FRAGMENT,             weight =   500 },
+        { itemId = xi.item.DEMON_HORN,                 weight =   500 },
+        { itemId = xi.item.HANDFUL_OF_WYVERN_SCALES,   weight =   500 },
+        { itemId = xi.item.RAM_HORN,                   weight =   500 },
+        { itemId = xi.item.SLAB_OF_GRANITE,            weight =   500 },
+        { itemId = xi.item.RERAISER,                   weight =   500 },
+        { itemId = xi.item.HI_RERAISER,                weight =   500 },
+        { itemId = xi.item.VILE_ELIXIR,                weight =   500 },
+        { itemId = xi.item.VILE_ELIXIR_P1,             weight =   500 },
     },
 
     {
-        { itemId = xi.item.ADAMAN_CHAIN,     weight = 268 }, -- Adaman Chain
-        { itemId = xi.item.ADAMANTOISE_EGG,  weight = 121 }, -- Adamantoise Egg
-        { itemId = xi.item.AGILITY_POTION,   weight =  80 }, -- Agility Potion
-        { itemId = xi.item.DEXTERITY_POTION, weight = 143 }, -- Dexterity Potion
-        { itemId = xi.item.STRENGTH_POTION,  weight = 214 }, -- Strength Potion
-        { itemId = xi.item.VITALITY_POTION,  weight = 196 }, -- Vitality Potion
+        { itemId = xi.item.ADAMANTOISE_EGG,            weight =  2500 },
+        { itemId = xi.item.ADAMAN_CHAIN,               weight =  3000 },
+        { itemId = xi.item.AGILITY_POTION,             weight =  1125 },
+        { itemId = xi.item.DEXTERITY_POTION,           weight =  1125 },
+        { itemId = xi.item.STRENGTH_POTION,            weight =  1125 },
+        { itemId = xi.item.VITALITY_POTION,            weight =  1125 },
     },
 
     {
-        { itemId = xi.item.CHUNK_OF_ADAMAN_ORE,     weight = 107 }, -- Chunk Of Adaman Ore
-        { itemId = xi.item.CHARISMA_POTION,         weight =  89 }, -- Charisma Potion
-        { itemId = xi.item.CHUNK_OF_DARKSTEEL_ORE,  weight = 179 }, -- Chunk Of Darksteel Ore
-        { itemId = xi.item.ICARUS_WING,             weight = 134 }, -- Icarus Wing
-        { itemId = xi.item.INTELLIGENCE_POTION,     weight = 152 }, -- Intelligence Potion
-        { itemId = xi.item.MIND_POTION,             weight =  80 }, -- Mind Potion
-        { itemId = xi.item.CHUNK_OF_ORICHALCUM_ORE, weight =  80 }, -- Chunk Of Orichalcum Ore
-        { itemId = xi.item.CHUNK_OF_PLATINUM_ORE,   weight = 107 }, -- Chunk Of Platinum Ore
-        { itemId = xi.item.PRINCELY_SWORD,          weight = 152 }, -- Princely Sword
+        { itemId = xi.item.MIND_POTION,                weight =  1250 },
+        { itemId = xi.item.INTELLIGENCE_POTION,        weight =  1250 },
+        { itemId = xi.item.CHARISMA_POTION,            weight =  1250 },
+        { itemId = xi.item.ICARUS_WING,                weight =  1250 },
+        { itemId = xi.item.CHUNK_OF_ADAMAN_ORE,        weight =   625 },
+        { itemId = xi.item.CHUNK_OF_DARKSTEEL_ORE,     weight =   625 },
+        { itemId = xi.item.CHUNK_OF_ORICHALCUM_ORE,    weight =   625 },
+        { itemId = xi.item.CHUNK_OF_PLATINUM_ORE,      weight =   625 },
+        { itemId = xi.item.PRINCELY_SWORD,             weight =  2500 },
     },
 
     {
-        { itemId = xi.item.HI_ETHER_P3,    weight = 295 },  -- Hi-ether +3
-        { itemId = xi.item.HI_POTION_P3,   weight = 250 },  -- Hi-potion +3
-        { itemId = xi.item.HI_RERAISER,    weight = 196 },  -- Hi-reraiser
-        { itemId = xi.item.VILE_ELIXIR_P1, weight = 214 },  -- Vile Elixir +1
+        { itemId = xi.item.HI_ETHER_P3,                weight =  2500 },
+        { itemId = xi.item.HI_POTION_P3,               weight =  2500 },
+        { itemId = xi.item.HI_RERAISER,                weight =  2500 },
+        { itemId = xi.item.VILE_ELIXIR_P1,             weight =  2500 },
     },
 
     {
-        { itemId = xi.item.VIAL_OF_BLACK_BEETLE_BLOOD, weight = 109 }, -- Vial Of Black Beetle Blood
-        { itemId = xi.item.SQUARE_OF_DAMASCENE_CLOTH,  weight =  89 }, -- Square Of Damascene Cloth
-        { itemId = xi.item.DAMASCUS_INGOT,             weight =  79 }, -- Damascus Ingot
-        { itemId = xi.item.SPOOL_OF_MALBORO_FIBER,     weight =  99 }, -- Spool Of Malboro Fiber
-        { itemId = xi.item.PHILOSOPHERS_STONE,         weight = 188 }, -- Philosophers Stone
-        { itemId = xi.item.PHOENIX_FEATHER,            weight = 238 }, -- Phoenix Feather
-        { itemId = xi.item.SQUARE_OF_RAXA,             weight = 109 }, -- Square Of Raxa
+        { itemId = xi.item.VIAL_OF_BLACK_BEETLE_BLOOD, weight =   625 },
+        { itemId = xi.item.SQUARE_OF_DAMASCENE_CLOTH,  weight =   625 },
+        { itemId = xi.item.DAMASCUS_INGOT,             weight =   625 },
+        { itemId = xi.item.SPOOL_OF_MALBORO_FIBER,     weight =   625 },
+        { itemId = xi.item.PHILOSOPHERS_STONE,         weight =  2000 },
+        { itemId = xi.item.PHOENIX_FEATHER,            weight =  3500 },
+        { itemId = xi.item.SQUARE_OF_RAXA,             weight =  2000 },
     },
 
     {
-        { itemId = xi.item.DIVINE_LOG,              weight =  79 }, -- Divine Log
-        { itemId = xi.item.LACQUER_TREE_LOG,        weight = 257 }, -- Lacquer Tree Log
-        { itemId = xi.item.PETRIFIED_LOG,           weight = 337 }, -- Petrified Log
-        { itemId = xi.item.SQUARE_OF_SHINING_CLOTH, weight = 149 }, -- Square Of Shining Cloth
+        { itemId = xi.item.DIVINE_LOG,                 weight =  1000 },
+        { itemId = xi.item.LACQUER_TREE_LOG,           weight =  2500 },
+        { itemId = xi.item.PETRIFIED_LOG,              weight =  6000 },
+        { itemId = xi.item.SQUARE_OF_SHINING_CLOTH,    weight =   500 },
     },
 }
 
