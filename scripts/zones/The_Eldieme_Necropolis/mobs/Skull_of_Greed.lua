760bf5a3c26945cdf6a6ce8a23e1fcec59ef8e07
ThrisStraizo <45871917+ThrisStraizo@users.noreply.github.com>
2026-04-14 20:46:52 -0700

    Skull of Sins Audit
    This PR audits the 7 Skulls of Sin for HP/Immunities/STP and general behavior.
    
    This also adds the missing gravity spell to the undead spell list, because it do be that way.

--- scripts/zones/The_Eldieme_Necropolis/mobs/Skull_of_Greed.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Skull_of_Greed.lua
@@ -5,6 +5,15 @@
 ---@type TMobEntity
 local entity = {}
 
+entity.onMobInitialize = function(mob)
+    mob:addImmunity(xi.immunity.DARK_SLEEP)
+end
+
+entity.onMobSpawn = function(mob)
+    mob:setMod(xi.mod.TRIPLE_ATTACK, 5)
+    mob:setMod(xi.mod.STORETP, 50)
+end
+
 entity.onMobDeath = function(mob, player, optParams)
     if player then
         player:addTitle(xi.title.SKULLCRUSHER)
--- scripts/zones/The_Eldieme_Necropolis/mobs/Skull_of_Pride.lua
+++ scripts/zones/The_Eldieme_Necropolis/mobs/Skull_of_Pride.lua
@@ -5,6 +5,16 @@
 ---@type TMobEntity
 local entity = {}
 
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.NO_STANDBACK, 1)
+    mob:addImmunity(xi.immunity.DARK_SLEEP)
+end
+
+entity.onMobSpawn = function(mob)
+    mob:setMod(xi.mod.TRIPLE_ATTACK, 5)
+    mob:setMod(xi.mod.STORETP, 50)
+end
+
 entity.onMobDeath = function(mob, player, optParams)
     if player then
         player:addTitle(xi.title.SKULLCRUSHER)
