2ded4c9f3e10035c350622f1d601c632771db8f4
Zach Toogood <zrtoogood@gmail.com>
2026-05-07 17:33:13 +0100

    Core: Clean up Vector3

--- src/map/ximesh/vector3.h
+++ src/map/ximesh/vector3.h
@@ -31,12 +31,12 @@ struct Vector3
     float y;
     float z;
 
-    Vector3 operator+(const Vector3& vec) const
+    constexpr Vector3 operator+(const Vector3& vec) const
     {
         return Vector3{ x + vec.x, y + vec.y, z + vec.z };
     }
 
-    Vector3& operator+=(const Vector3& vec)
+    constexpr Vector3& operator+=(const Vector3& vec)
     {
         x += vec.x;
         y += vec.y;
@@ -44,12 +44,12 @@ struct Vector3
         return *this;
     }
 
-    Vector3 operator-(const Vector3& vec) const
+    constexpr Vector3 operator-(const Vector3& vec) const
     {
         return Vector3{ x - vec.x, y - vec.y, z - vec.z };
     }
 
-    Vector3& operator-=(const Vector3& vec)
+    constexpr Vector3& operator-=(const Vector3& vec)
     {
         x -= vec.x;
         y -= vec.y;
@@ -57,12 +57,12 @@ struct Vector3
         return *this;
     }
 
-    Vector3 operator*(float value) const
+    constexpr Vector3 operator*(float value) const
     {
         return Vector3{ x * value, y * value, z * value };
     }
 
-    Vector3& operator*=(float value)
+    constexpr Vector3& operator*=(float value)
     {
         x *= value;
         y *= value;
@@ -70,12 +70,12 @@ struct Vector3
         return *this;
     }
 
-    Vector3 operator/(float value) const
+    constexpr Vector3 operator/(float value) const
     {
         return Vector3{ x / value, y / value, z / value };
     }
 
-    Vector3& operator/=(float value)
+    constexpr Vector3& operator/=(float value)
     {
         x /= value;
         y /= value;
@@ -83,34 +83,28 @@ struct Vector3
         return *this;
     }
 
-    Vector3& operator=(const Vector3& vec)
+    constexpr auto crossProduct(const Vector3& other) const -> Vector3
     {
-        x = vec.x;
-        y = vec.y;
-        z = vec.z;
-        return *this;
+        return Vector3{
+            y * other.z - z * other.y,
+            z * other.x - x * other.z,
+            x * other.y - y * other.x,
+        };
     }
 
-    Vector3 crossProduct(const Vector3& other) const
-    {
-        float ni = y * other.z - z * other.y;
-        float nj = z * other.x - x * other.z;
-        float nk = x * other.y - y * other.x;
-        return Vector3{ ni, nj, nk };
-    }
-
-    float dotProduct(const Vector3& other) const
+    constexpr auto dotProduct(const Vector3& other) const -> float
     {
         return x * other.x + y * other.y + z * other.z;
     }
 
-    float magnitude() const
-    {
-        return std::sqrt(x * x + y * y + z * z);
-    }
-
-    float magnitudeSquared() const
+    constexpr auto magnitudeSquared() const -> float
     {
         return x * x + y * y + z * z;
+    }
+
+    // NOTE: std::sqrt is only constexpr in C++23
+    auto magnitude() const -> float
+    {
+        return std::sqrt(magnitudeSquared());
     }
 };
