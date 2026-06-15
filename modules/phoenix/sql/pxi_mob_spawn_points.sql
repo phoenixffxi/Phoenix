e6dacee9875c0aff5c353aa3204beca9c331e53b
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-06-14 21:49:48 -0400

    Crawlers Nest era mobs

---
+++ modules/phoenix/sql/pxi_mob_spawn_points.sql
@@ -0,0 +1,90 @@
+REPLACE INTO `mob_spawn_points` VALUES (17584214, 0, 'Worker_Crawler', 'Worker Crawler', 6, 41, 43, 98.2741, -28.0657, -218.6358, 68);
+REPLACE INTO `mob_spawn_points` VALUES (17584215, 0, 'Worker_Crawler', 'Worker Crawler', 6, 41, 43, 122.2749, -23.6800, -220.2118, 253);
+REPLACE INTO `mob_spawn_points` VALUES (17584216, 0, 'Worker_Crawler', 'Worker Crawler', 6, 41, 43, 135.7329, -20.4805, -221.7728, 194);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584217, 0, 'Worker_Crawler', 'Worker Crawler', 6, 41, 43, 127.1174, -9.2481, -178.0998, 134);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584218, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 93.7685, -0.6801, -232.4239, 80);
+REPLACE INTO `mob_spawn_points` VALUES (17584219, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 128.6015, -0.9256, -229.8730, 145);
+REPLACE INTO `mob_spawn_points` VALUES (17584220, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 88.5284, -0.5404, -251.1405, 55);
+REPLACE INTO `mob_spawn_points` VALUES (17584199, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 98.6550, -0.6478, -246.8551, 46);
+REPLACE INTO `mob_spawn_points` VALUES (17584200, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 122.9013, -0.1911, -244.9688, 240);
+REPLACE INTO `mob_spawn_points` VALUES (17584201, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 104.6843, -0.0920, -242.6987, 53);
+REPLACE INTO `mob_spawn_points` VALUES (17584221, 0, 'Exoray', 'Exoray', 15, 51, 53, 115.0242, -0.2254, -252.9007, 236);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584222, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 105.0773, -0.8995, -304.9634, 178);
+REPLACE INTO `mob_spawn_points` VALUES (17584223, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 117.8891, -0.3902, -317.8894, 30);
+REPLACE INTO `mob_spawn_points` VALUES (17584224, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 134.2767, -0.6870, -311.1832, 221);
+REPLACE INTO `mob_spawn_points` VALUES (17584202, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 121.6798, -0.1079, -317.8589, 54);
+REPLACE INTO `mob_spawn_points` VALUES (17584203, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 104.3560, -0.2542, -332.1602, 90);
+REPLACE INTO `mob_spawn_points` VALUES (17584225, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 119.0346, -0.1795, -353.9108, 198);
+REPLACE INTO `mob_spawn_points` VALUES (17584204, 0, 'Exoray', 'Exoray', 15, 51, 53, 137.9511, -1.0136, -348.5273, 201);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584226, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 98.5077, -0.5385, -386.5239, 89);
+REPLACE INTO `mob_spawn_points` VALUES (17584227, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 103.8400, -1.0878, -417.6123, 3);
+REPLACE INTO `mob_spawn_points` VALUES (17584228, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 85.6533, -1.3913, -412.6092, 50);
+REPLACE INTO `mob_spawn_points` VALUES (17584205, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 120.6516, -0.0332, -399.2948, 138);
+REPLACE INTO `mob_spawn_points` VALUES (17584206, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 109.9671, -0.2749, -396.6920, 201);
+REPLACE INTO `mob_spawn_points` VALUES (17584208, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 88.6005, -0.0883, -399.0998, 93);
+REPLACE INTO `mob_spawn_points` VALUES (17584207, 0, 'Exoray', 'Exoray', 15, 51, 53, 129.5359, -0.2135, -400.0178, 35);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584229, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 30.4561, -0.7226, -410.1404, 159);
+REPLACE INTO `mob_spawn_points` VALUES (17584230, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 14.9297, -0.2547, -396.9289, 154);
+REPLACE INTO `mob_spawn_points` VALUES (17584231, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 6.1019, -0.1902, -401.7883, 112);
+REPLACE INTO `mob_spawn_points` VALUES (17584232, 0, 'Exoray', 'Exoray', 15, 51, 53, 10.5448, -0.6253, -416.7654, 242);
+REPLACE INTO `mob_spawn_points` VALUES (17584298, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, 64.1742, -0.5200, -411.3162, 127);
+REPLACE INTO `mob_spawn_points` VALUES (17584299, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, 52.7669, -0.3802, -408.1842, 140);
+REPLACE INTO `mob_spawn_points` VALUES (17584300, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, 32.9418, -1.1859, -415.2813, 107);
+REPLACE INTO `mob_spawn_points` VALUES (17584233, 0, 'Mushussu', 'Mushussu', 30, 56, 57, 20.7785, -0.6703, -391.8500, 202);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584234, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -27.9478, -0.2826, -415.8047, 112);
+REPLACE INTO `mob_spawn_points` VALUES (17584235, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -29.8983, -0.6773, -369.5570, 183);
+REPLACE INTO `mob_spawn_points` VALUES (17584236, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -41.6739, -0.1005, -368.6927, 142);
+REPLACE INTO `mob_spawn_points` VALUES (17584237, 0, 'Exoray', 'Exoray', 15, 51, 53, -44.7259, -0.4818, -376.2353, 98);
+REPLACE INTO `mob_spawn_points` VALUES (17584303, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, -25.9000, -0.0382, -402.2396, 193);
+REPLACE INTO `mob_spawn_points` VALUES (17584304, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, -18.2570, -1.7303, -379.1053, 254);
+REPLACE INTO `mob_spawn_points` VALUES (17584238, 0, 'Blazer_Beetle', 'Blazer Beetle', 22, 52, 54, -44.0674, -0.5193, -390.8338, 42);
+REPLACE INTO `mob_spawn_points` VALUES (17584239, 0, 'Mushussu', 'Mushussu', 30, 56, 57, -56.5970, -0.8744, -367.4872, 172);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584240, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -49.5033, -0.3954, -334.3723, 190);
+REPLACE INTO `mob_spawn_points` VALUES (17584241, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -21.8552, -0.9333, -313.5030, 233);
+REPLACE INTO `mob_spawn_points` VALUES (17584242, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -55.6436, -0.5442, -297.3746, 78);
+REPLACE INTO `mob_spawn_points` VALUES (17584243, 0, 'Exoray', 'Exoray', 15, 51, 53, -48.0566, -1.0392, -326.6586, 200);
+REPLACE INTO `mob_spawn_points` VALUES (17584307, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, -34.9521, -0.2810, -317.1544, 210);
+REPLACE INTO `mob_spawn_points` VALUES (17584308, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, -34.7098, -0.5216, -295.5239, 152);
+REPLACE INTO `mob_spawn_points` VALUES (17584309, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, -46.6150, -0.2047, -282.4393, 179);
+REPLACE INTO `mob_spawn_points` VALUES (17584244, 0, 'Mushussu', 'Mushussu', 30, 56, 57, -33.1268, -1.2648, -291.1161, 3);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584245, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -46.2618, -0.3797, -265.2280, 198);
+REPLACE INTO `mob_spawn_points` VALUES (17584246, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -18.7028, -1.5399, -260.3474, 253);
+REPLACE INTO `mob_spawn_points` VALUES (17584247, 0, 'Hornfly', 'Hornfly', 14, 51, 53, -12.2571, -0.2851, -245.3293, 195);
+REPLACE INTO `mob_spawn_points` VALUES (17584248, 0, 'Exoray', 'Exoray', 15, 51, 53, -13.8862, -0.0593, -225.5699, 158);
+REPLACE INTO `mob_spawn_points` VALUES (17584310, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, -1.2128, -0.0869, -226.9176, 12);
+REPLACE INTO `mob_spawn_points` VALUES (17584311, 0, 'BlazerBeetle', 'BlazerBeetle', 0, 52, 54, -26.0541, -0.2829, -250.4424, 136);
+REPLACE INTO `mob_spawn_points` VALUES (17584249, 0, 'Blazer_Beetle', 'Blazer Beetle', 22, 52, 54, -28.8292, -0.7758, -267.4775, 88);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584250, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 67.3385, -0.2200, -229.2056, 252);
+REPLACE INTO `mob_spawn_points` VALUES (17584251, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 39.1476, -0.0695, -253.2754, 11);
+REPLACE INTO `mob_spawn_points` VALUES (17584252, 0, 'Soldier_Crawler', 'Soldier Crawler', 13, 47, 49, 14.9464, -0.3127, -245.5538, 242);
+REPLACE INTO `mob_spawn_points` VALUES (17584253, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 74.4010, -0.1210, -245.0075, 38);
+REPLACE INTO `mob_spawn_points` VALUES (17584254, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 42.6802, -0.2185, -235.4413, 211);
+REPLACE INTO `mob_spawn_points` VALUES (17584255, 0, 'Hornfly', 'Hornfly', 14, 51, 53, 29.1106, -1.1740, -246.4425, 25);
+REPLACE INTO `mob_spawn_points` VALUES (17584256, 0, 'Exoray', 'Exoray', 15, 51, 53, 18.3480, -0.4822, -231.0742, 188);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584209, 0, 'Exoray', 'Exoray', 15, 51, 53, 6.1090, -0.1234, -299.0036, 4);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584210, 0, 'Exoray', 'Exoray', 15, 51, 53, 30.0126, -0.3564, -302.7664, 220);
+REPLACE INTO `mob_spawn_points` VALUES (17584211, 0, 'Exoray', 'Exoray', 15, 51, 53, 27.7411, -0.9379, -316.0207, 77);
+REPLACE INTO `mob_spawn_points` VALUES (17584212, 0, 'Exoray', 'Exoray', 15, 51, 53, 27.0318, -0.9329, -327.1567, 72);
+REPLACE INTO `mob_spawn_points` VALUES (17584213, 0, 'Exoray', 'Exoray', 15, 51, 53, 45.1757, -0.1785, -316.6555, 202);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584389, 0, 'Rumble_Crawler', 'Rumble Crawler', 31, 53, 55, 0, 0, 0, 0);
+REPLACE INTO `mob_spawn_points` VALUES (17584390, 0, 'Rumble_Crawler', 'Rumble Crawler', 31, 53, 55, 0, 0, 0, 0);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584257, 0, 'HelmBeetle', 'HelmBeetle', 0, 59, 62, -218.4542, -0.1733, -275.8495, 65);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584258, 0, 'CrawlerHunter', 'CrawlerHunter', 0, 60, 62, -247.5350, -0.4927, -251.0122, 109);
+REPLACE INTO `mob_spawn_points` VALUES (17584259, 0, 'CrawlerHunter', 'CrawlerHunter', 0, 60, 62, -243.2216, -0.1676, -234.5039, 185);
+REPLACE INTO `mob_spawn_points` VALUES (17584378, 0, 'Mushussu', 'Mushussu', 30, 0, 0, 48.3845, -0.4743, 367.8715, 177);
+
+REPLACE INTO `mob_spawn_points` VALUES (17584321, 0, 'KnightCrawler', 'KnightCrawler', 0, 60, 63, -297.8918, -0.4755, -256.9619, 116);
