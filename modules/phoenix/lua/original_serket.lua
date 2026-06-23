3f6dd5987d6170eda5abef86cffab0fe6b4c2b69
Skold <113406182+Skold177@users.noreply.github.com>
2026-06-22 23:45:09 -0400

    Phoenix module fixes
    Fixes two old modules and updates them to work with changes in the code base.

--- modules/phoenix/lua/original_serket.lua
+++ modules/phoenix/lua/original_serket.lua
@@ -5,30 +5,33 @@ require('modules/module_utils')
 -----------------------------------
 local m = Module:new('original_serket')
 
+local skillAnimations =
+{
+    [xi.mobSkill.VENOM_STING_1 ] = 104,
+    [xi.mobSkill.VENOM_STORM_1 ] = 107,
+    [xi.mobSkill.VENOM_BREATH_1] = 101,
+    [xi.mobSkill.CRITICAL_BITE ] = 103,
+    [xi.mobSkill.EARTHBREAKER_1] = 108,
+    [xi.mobSkill.STASIS        ] = 106,
+    [xi.mobSkill.EVASION       ] = 109,
+}
+
 m:addOverride('xi.zones.Garlaige_Citadel.mobs.Serket.onMobInitialize', function(mob)
     super(mob)
     mob:setModelId(285)
 
-    mob:addListener('WEAPONSKILL_USE', 'SERKET_WEAPONSKILL_USE', function(mobArg, target, skillid, tp, action)
+    mob:addListener('WEAPONSKILL_USE', 'SERKET_WEAPONSKILL_USE', function(mobArg, target, skill, tp, action, damage)
         local targetId = target and target:getID() or nil
-        if targetId == nil then
+        if
+            targetId == nil or
+            skill == nil
+        then
             return
         end
 
-        if skillid == xi.mobSkill.VENOM_STING_1 then
-            action:setAnimation(targetId, 104)
-        elseif skillid == xi.mobSkill.VENOM_STORM_1 then
-            action:setAnimation(targetId, 107)
-        elseif skillid == xi.mobSkill.VENOM_BREATH_1 then
-            action:setAnimation(targetId, 101)
-        elseif skillid == xi.mobSkill.CRITICAL_BITE then
-            action:setAnimation(targetId, 103)
-        elseif skillid == xi.mobSkill.EARTHBREAKER_1 then
-            action:setAnimation(targetId, 108)
-        elseif skillid == xi.mobSkill.STASIS then
-            action:setAnimation(targetId, 106)
-        elseif skillid == xi.mobSkill.EVASION then
-            action:setAnimation(targetId, 109)
+        local animationId = skillAnimations[skill:getID()]
+        if animationId then
+            action:setAnimation(targetId, animationId)
         end
     end)
 end)
