4634f956b9f7f78b867e0c53d6e45f510d7ddb3d
Skold <113406182+Skold177@users.noreply.github.com>
2026-06-27 00:33:46 -0400

    Temenos - Northern Tower Audit
    Audits mobs inside of Temenos Northern Tower, fixes some model issues with treasure chests. Adds hit boxes, fixes delay, skill lists etc.

--- scripts/zones/Temenos/mobs/Kindred_Warrior.lua
+++ scripts/zones/Temenos/mobs/Kindred_Warrior.lua
@@ -7,7 +7,9 @@ mixins = { require('scripts/mixins/job_special') }
 ---@type TMobEntity
 local entity = {}
 
-entity.onMobDeath = function(mob, player, optParams)
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.BASE_DAMAGE_MULTIPLIER, 150)
+    mob:setMod(xi.mod.UDMGMAGIC, -2500)
 end
 
 return entity
